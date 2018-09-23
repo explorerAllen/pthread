965aa6677b992f004efa77b049e605626bbf41e6
explorerAllen <explorer.pingzhao@foxmail.com>
2018-09-23 11:26:05 +0200

    添加第一个例程,说明线程的生命周期

---
+++ pthread.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+
+void *thread_fun(void *arg)
+{
+    printf("thread id: %ld\n", pthread_self());
+
+    return NULL;
+}
+
+
+int main(void)
+{
+    int ret = 0;
+    pthread_t thread_id;
+ 
+    ret = pthread_create(&thread_id, NULL, thread_fun, NULL);
+    if (ret != 0)
+    {
+        printf("pthread_creat failed, error num: %d\n", ret);
+        printf("%s\n", strerror(ret));
+        goto ERR;
+    }
+    printf("chi thread id: %ld\n", thread_id); 
+    
+    sleep(2);  // 测试主线程的生命周期和子线程生命周期的关系
+    printf("main thread id: %ld\n", pthread_self());
+
+ERR:
+    return ret;
+}
+
